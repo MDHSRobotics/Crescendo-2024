bae3a2a5610940e7bf38b1a061d8223c762dd50d
TJ Vasquez <vasquezt.24@stu.materdei.org>
2024-01-09 16:49:00 -0800

    Actual Base Commit

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -11,7 +11,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 
 
-public class TeleopSwerve extends Command {    
+public class TeleopSwerve extends Command{    
     private Swerve s_Swerve;    
     private DoubleSupplier translationSup;
     private DoubleSupplier strafeSup;
