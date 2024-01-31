b17015fb60888eef7fc38ca52538a4ccc40f6572
Paul Sicking <sickingp@gmail.com>
2024-01-31 12:26:56 -0800

    Configure Path Planner Auto Builder

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -8,11 +8,17 @@ import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
+import com.pathplanner.lib.auto.*;
+import com.pathplanner.lib.util.*;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 /**
  * Class that extends the Phoenix SwerveDrivetrain class and implements subsystem
  * so it can be used in command-based projects easily.
@@ -27,18 +33,74 @@ public class Swerve extends SwerveDrivetrain implements Subsystem {
         if (Utils.isSimulation()) {
             startSimThread();
         }
+
+        // Configure AutoBuilder last
+        configurePathPlanningAutoBuilder();
     }
     public Swerve(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
         super(driveTrainConstants, modules);
         if (Utils.isSimulation()) {
             startSimThread();
         }
+
+        // Configure AutoBuilder last
+        configurePathPlanningAutoBuilder();
     }
 
+    private void configurePathPlanningAutoBuilder() {
+
+        AutoBuilder.configureHolonomic(
+                    this::getPose, // Robot pose supplier
+                    this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+                    this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                    this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+                    new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+                            new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                            new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+                            4.5, // Max module speed, in m/s
+                            0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                            new ReplanningConfig() // Default path replanning config. See the API for the options here
+                    ),
+                    () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red alliance
+                    // This will flip the path being followed to the red side of the field.
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                    },
+                    this // Reference to this subsystem to set requirements
+            );
+
+    }
+
+    // Apply a request to the swerve subsystem
     public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
         return run(() -> this.setControl(requestSupplier.get()));
     }
 
+    // The following are callbacks needed for the Path Planner Auto Builder
+
+    private Pose2d getPose() {
+        return this.getPose();
+    }
+
+    private Pose2d resetPose(Pose2d newPose) {
+        return this.resetPose(newPose);
+    }
+
+    private ChassisSpeeds getRobotRelativeSpeeds(){
+        return this.getRobotRelativeSpeeds();
+    }
+
+    public void driveRobotRelative(ChassisSpeeds speeds){
+        this.driveRobotRelative(speeds);
+      }
+
+    // Simulation
     private void startSimThread() {
         m_lastSimTime = Utils.getCurrentTimeSeconds();
 
