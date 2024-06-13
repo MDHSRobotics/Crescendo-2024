26045bd35c36a6186c2a85d8212a37193ba3907a
Anthony Pagone <anthony21@pagone.com>
2024-06-12 21:03:43 -0700

    Removed more old comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,9 +10,6 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-//import frc.robot.consoles.Logger;
-//import frc.robot.subsystems.Pathweaver; 
-//import frc.robot.consoles.Shuffler;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
