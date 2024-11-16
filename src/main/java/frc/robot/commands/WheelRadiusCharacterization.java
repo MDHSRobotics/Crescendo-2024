14d5702d01f5f5b2307453269cf6f21e0fa1929e
Anthony Pagone <anthony21@pagone.com>
2024-11-15 16:05:18 -0800

    Added warning to WheelRadiusCharacterization

--- src/main/java/frc/robot/commands/WheelRadiusCharacterization.java
+++ src/main/java/frc/robot/commands/WheelRadiusCharacterization.java
@@ -16,6 +16,7 @@ import frc.robot.subsystems.Swerve;
 /**
  * This command rotates the robot, and uses the gyro to calculate the average radius of the wheels.
  * This can be used in TunerConstants.kWheelRadiusInches to make our odometry more accurate.
+ * To be sure that the steer motor position does not cause any inaccuracies, run this as soon as you turn on the robot.
  * All credit goes to Team 6328 Mechanical Advantage for this command.
  * @see <a href="https://www.chiefdelphi.com/t/frc-6328-mechanical-advantage-2024-build-thread/442736/264#wheel-radius-characterization-7">Mechanical Advantage's explanation</a>
  * @see <a href="https://github.com/Mechanical-Advantage/RobotCode2024/blob/main/src/main/java/org/littletonrobotics/frc2024/commands/WheelRadiusCharacterization.java">Mechanical Advantage's code</a>
